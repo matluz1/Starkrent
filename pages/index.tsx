f3d6b86f46e70a24e39c19fead1385b377f8884f
0xErenes <matluz994@gmail.com>
2022-12-15 23:25:09 -0300

    add social icons and links

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,17 @@ export default function Home() {
   return (
     <section className={styles.home}>
       <main className={styles.main}>
-        <Image
-          src="/logo.svg"
-          alt="Starkrent lion logo"
-          width={150}
-          height={150}
-        />
         <div className={styles.content}>
           <Image
+            className={styles.logo}
+            src="/logo.svg"
+            alt="Starkrent lion logo"
+            width={150}
+            height={150}
+          />
+          <Image
             src="/starkrent.svg"
-            className=".image"
-            alt="Starkrent"
+            alt="Starkrent logotype"
             width={500}
             height={50}
           />
@@ -26,7 +26,17 @@ export default function Home() {
         <Link href="/app/rent">Launch app</Link>
       </main>
 
-      <footer className={styles.footer}>{/* add social icons */}</footer>
+      <footer className={styles.footer}>
+        <a href="https://www.twitter.com" target="blank">
+          <Image src="/twitter.svg" alt="Twitter icon" width={30} height={30} />
+        </a>
+        <a href="https://www.discord.com" target="blank">
+          <Image src="/discord.svg" alt="Discord icon" width={30} height={30} />
+        </a>
+        <a href="https://www.medium.com" target="blank">
+          <Image src="/medium.svg" alt="Medium icon" width={30} height={30} />
+        </a>
+      </footer>
     </section>
   );
 }
