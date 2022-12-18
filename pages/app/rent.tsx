7e01dc562708b25a43a4299d92fe70223dcb2d89
0xErenes <0xerenes@gmail.com>
2022-12-17 22:00:29 -0300

    add collection selection pattern

--- pages/app/rent.tsx
+++ pages/app/rent.tsx
@@ -1,13 +1,56 @@
 import { useRouter } from 'next/router';
+import Image from 'next/image';
+import styles from '../../styles/Rent.module.scss';
+import Link from 'next/link';
+
+const socialIconSize = 20;
 
 const Home = () => {
   const router = useRouter();
 
   return (
-    <div>
-      <h1>My Next.js Component</h1>
-      <p>You are currently on: {router.pathname}</p>
-    </div>
+    <section className={styles.rent}>
+      <h1>Select your Collection</h1>
+      <div className={styles.collectionWrapper}>
+        <Link href="/app/rent/collection" className={styles.collection}>
+          <div className={styles.collectionHeader}>
+            <Image
+              className={styles.profileImage}
+              src="/placeholder.png"
+              alt="Collection name"
+              width={70}
+              height={70}
+            />
+            <h3>CollectionName</h3>
+          </div>
+          <div className={styles.collectionContent}>
+            <span>test</span>
+            <div className={styles.socials}>
+              <Image
+                src="/twitter.svg"
+                alt="Twitter icon"
+                width={socialIconSize}
+                height={socialIconSize}
+              />
+              <Image
+                src="/discord.svg"
+                alt="Discord icon"
+                width={socialIconSize}
+                height={socialIconSize}
+              />
+            </div>
+          </div>
+          <button className={styles.toggleContent}>
+            <Image
+              src="/plus.svg"
+              alt="Show more"
+              width={socialIconSize}
+              height={socialIconSize}
+            />
+          </button>
+        </Link>
+      </div>
+    </section>
   );
 };
 
