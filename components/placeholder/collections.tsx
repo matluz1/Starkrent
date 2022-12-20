71c716b0ecf953414057008e38b82f6542a629f1
0xErenes <0xerenes@gmail.com>
2022-12-20 00:10:39 -0300

    add rent collection layout and show/hide description function

---
+++ components/placeholder/collections.tsx
@@ -0,0 +1,52 @@
+const collections = [
+  {
+    name: 'My Collection',
+    icon: 'https://example.com/icon.png',
+    description:
+      'Cras fringilla, justo non commodo varius, nisi arcu ultrices purus, sit amet congue nisi quam faucibus quam. Nunc fringilla suscipit turpis sed tempus. Vestibulum turpis.',
+    address: '0x671817E82fd2BD62Ad441d901077C04601d012De', //random address
+    twitter: 'https://twitter.com/mycollection',
+  },
+  {
+    name: 'Another Collection',
+    icon: 'https://example.com/icon2.png',
+    description: 'Vivamus in tincidunt tellus, a placerat felis. In id felis vel erat porta auctor at vel libero. Maecenas bibendum, massa.',
+    address: '0xa3595A343CE794063864Efa845a75eacFaCf0F1E', //random address
+    discord: 'https://discord.com/anothercollection',
+    medium: 'https://medium.com/anothercollection',
+  },
+  {
+    name: 'Yet Another Collection',
+    icon: 'https://example.com/icon3.png',
+    description: 'Praesent at ipsum sem. In hac habitasse platea dictumst. Vestibulum congue nulla ac euismod elementum. In in orci lacus. Vivamus.',
+    address: '0x3b3f8a2B2A1BdE6A4b6B4A6A4c6Ae9bBc7Ae8bBd', //random address
+    twitter: 'https://twitter.com/yetanothercollection',
+    discord: 'https://discord.com/yetanothercollection',
+  },
+  {
+    name: 'My Collection 4',
+    icon: 'https://example.com/icon.png',
+    description: 'Proin fringilla ultricies convallis. Praesent orci neque, laoreet ornare blandit et, volutpat quis massa. Ut scelerisque, sem vel convallis bibendum, diam est gravida sem, at tristique dui massa sed sem. Vivamus non bibendum neque. Sed congue, augue in faucibus rhoncus, elit nunc tincidunt nisi, non.',
+    address: '0x671817E82fd2BD62Ad441d901077C03601d012De', //random address
+    twitter: 'https://twitter.com/mycollection',
+  },
+  {
+    name: 'Another Collection 5',
+    icon: 'https://example.com/icon2.png',
+    description: 'This is another collection of things',
+    address: '0xa3595A343CE794053864Efa845a75eacFaCf0F1E', //random address
+    twitter: 'https://twitter.com/anothercollection',
+    discord: 'https://discord.com/anothercollection',
+    medium: 'https://medium.com/anothercollection',
+  },
+  {
+    name: 'Yet Another Collection 6',
+    icon: 'https://example.com/icon3.png',
+    description: 'This is yet another collection of things',
+    address: '0x3b3f8a2B2B1BdE6A4b6B4A6A4c6Ae9bBc7Ae8bBd', //random address
+    twitter: 'https://twitter.com/yetanothercollection',
+    discord: 'https://discord.com/yetanothercollection',
+  },
+];
+
+export default collections;
--- pages/app/rent.tsx
+++ pages/app/rent.tsx
@@ -1,57 +1,132 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 import styles from '../../styles/Rent.module.scss';
-import Link from 'next/link';
+import collections from '../../components/placeholder/collections';
 
 const socialIconSize = 20;
+const toggleDescriptionButtonSize = 12;
 
-const Home = () => {
+interface Collection {
+  name: string;
+  icon: string;
+  description: string;
+  showingDescription?: boolean;
+  address: string;
+  twitter?: string;
+  discord?: string;
+  medium?: string;
+}
+
+type showDescriptionState = {
+  [address: string]: boolean;
+};
+
+const collectionArray: Collection[] = collections;
+
+function getCollectionDescription(collectionInfo: Collection) {
+  return (
+    <div className={styles.collectionDescription}>
+      <span>{collectionInfo.description}</span>
+      <div className={styles.socials}>
+        {collectionInfo.twitter && (
+          <a href={collectionInfo.twitter} target="blank">
+            <Image
+              src="/twitter.svg"
+              alt="Twitter icon"
+              width={socialIconSize}
+              height={socialIconSize}
+            />
+          </a>
+        )}
+        {collectionInfo.discord && (
+          <a href={collectionInfo.discord} target="blank">
+            <Image
+              src="/discord.svg"
+              alt="Discord icon"
+              width={socialIconSize}
+              height={socialIconSize}
+            />
+          </a>
+        )}
+        {collectionInfo.medium && (
+          <a href={collectionInfo.medium} target="blank">
+            <Image
+              src="/medium.svg"
+              alt="Medium icon"
+              width={socialIconSize}
+              height={socialIconSize}
+            />
+          </a>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Rent() {
   const router = useRouter();
+  const [showDescription, setShowDescription] = useState<showDescriptionState>(
+    {},
+  );
+
+  function toggleDescription(address: string) {
+    setShowDescription({
+      ...showDescription,
+      [address]: !showDescription[address],
+    });
+  }
+
+  const handleClick = (address: string) => {
+    router.push(`/app/rent/${address}`);
+  };
 
   return (
     <section className={styles.rent}>
       <h1>Select your Collection</h1>
       <div className={styles.collectionWrapper}>
-        <Link href="/app/rent/collection" className={styles.collection}>
-          <div className={styles.collectionHeader}>
-            <Image
-              className={styles.profileImage}
-              src="/placeholder.png"
-              alt="Collection name"
-              width={70}
-              height={70}
-            />
-            <h3>CollectionName</h3>
-          </div>
-          <div className={styles.collectionContent}>
-            <span>test</span>
-            <div className={styles.socials}>
+        {collectionArray.map((collectionInfo) => (
+          <div className={styles.collection} key={collectionInfo.address}>
+            <button
+              className={styles.collectionTitle}
+              onClick={() => handleClick(collectionInfo.address)}
+            >
               <Image
-                src="/twitter.svg"
-                alt="Twitter icon"
-                width={socialIconSize}
-                height={socialIconSize}
+                className={styles.profileImage}
+                src="/placeholder.png"
+                alt="Collection name"
+                width={70}
+                height={70}
               />
-              <Image
-                src="/discord.svg"
-                alt="Discord icon"
-                width={socialIconSize}
-                height={socialIconSize}
-              />
-            </div>
+              <h3>{collectionInfo.name}</h3>
+            </button>
+            {showDescription[collectionInfo.address] &&
+              getCollectionDescription(collectionInfo)}
+            <button
+              className={styles.toggleDescription}
+              onClick={() => toggleDescription(collectionInfo.address)}
+            >
+              {showDescription[collectionInfo.address] ? (
+                <Image
+                  src="/minus.svg"
+                  alt="Show less"
+                  width={toggleDescriptionButtonSize}
+                  height={toggleDescriptionButtonSize}
+                />
+              ) : (
+                <Image
+                  src="/plus.svg"
+                  alt="Show more"
+                  width={toggleDescriptionButtonSize}
+                  height={toggleDescriptionButtonSize}
+                />
+              )}
+            </button>
           </div>
-          <button className={styles.toggleContent}>
-            <Image
-              src="/plus.svg"
-              alt="Show more"
-              width={socialIconSize}
-              height={socialIconSize}
-            />
-          </button>
-        </Link>
+        ))}
       </div>
     </section>
   );
-};
+}
 
-export default Home;
+export default Rent;
