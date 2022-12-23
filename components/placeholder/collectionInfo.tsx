3fc60339062eee21bcff5f381123002db88d470c
0xErenes <0xerenes@gmail.com>
2022-12-22 21:21:18 -0300

    add export default

--- components/placeholder/collectionInfo.tsx
+++ components/placeholder/collectionInfo.tsx
@@ -1,12 +1,14 @@
-
-[
+const collectionInfo = [
   {
-    address: '0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b',
+    address:
+      '0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b',
     info: {
       name: 'Starknet.id',
       profileImage: '/starknetid.jpg',
       description: 'Identity and naming service for StarkNet',
       twitter: 'https://twitter.com/Starknet_id',
-    }
-  }
-]
+    },
+  },
+];
+
+export default collectionInfo;
