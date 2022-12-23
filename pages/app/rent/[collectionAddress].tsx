a74c021facd6ce0e8ac187be09bf3ee651f0a2cc
0xErenes <0xerenes@gmail.com>
2022-12-22 21:18:00 -0300

    add collectionInfo

--- pages/app/rent/[collectionAddress].tsx
+++ pages/app/rent/[collectionAddress].tsx
@@ -1,7 +1,18 @@
-import { useRouter } from 'next/router'
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 export default function Page() {
-  const router = useRouter()
-  const { collectionAddress } = router.query
-  return <p>{collectionAddress}</p>
+  const router = useRouter();
+  const { collectionAddress } = router.query;
+  return (
+    <p>
+      {collectionAddress}
+      <Image
+        src="https://starknet.id/api/identicons/626200613416"
+        alt="Starkrent lion logo"
+        width={60}
+        height={60}
+      />
+    </p>
+  );
 }
