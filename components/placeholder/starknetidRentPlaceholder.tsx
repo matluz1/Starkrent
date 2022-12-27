9f27f123f263f42f2dfc11c25db83b48c9f9f035
0xErenes <0xerenes@gmail.com>
2022-12-27 20:23:44 -0300

    add more playeholder items and add initial layout to rent/[collectionAddress]

--- components/placeholder/starknetidRentPlaceholder.tsx
+++ components/placeholder/starknetidRentPlaceholder.tsx
@@ -5,8 +5,32 @@ const rentPlaceholder = [
       name: 'Starknet ID: 626200613416',
       description: 'This token represents an identity on StarkNet.',
       image: 'https://starknet.id/api/identicons/626200613416',
-    },
+    }
   },
+  {
+    id: '4047446163',
+    info: {
+      name: 'Starknet ID: 4047446163',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/4047446163',
+    }
+  },
+  {
+    id: '789793576154',
+    info: {
+      name: 'Starknet ID: 789793576154',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/789793576154',
+    }
+  },
+  {
+    id: '261880110824',
+    info: {
+      name: 'Starknet ID: 261880110824',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/261880110824',
+    }
+  }
 ];
 
 export default rentPlaceholder;
--- pages/app/rent/[collectionAddress].tsx
+++ pages/app/rent/[collectionAddress].tsx
@@ -1,19 +1,45 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import styles from '../../../styles/[collectionAddress].module.scss';
+import rentPlaceholder from '../../../components/placeholder/starknetidRentPlaceholder';
 
 export default function Page() {
   const router = useRouter();
   const { collectionAddress } = router.query;
+
   return (
-    <p>
-      {collectionAddress}
-      <Image
-        src="https://starknet.id/api/identicons/626200613416"
-        alt="Starkrent lion logo"
-        width={60}
-        height={60}
-        unoptimized //reason for the 'unoptimized': https://github.com/vercel/next.js/issues/42032
-      />
-    </p>
+    <>
+      {/* {collectionAddress} */}
+      <div className={styles.collectionItemWrapper}>
+        {rentPlaceholder.map((item) => (
+          <div className={styles.collectionItem}>
+            <div className={styles.itemImage}>
+              <Image
+                src={item.info.image}
+                alt={item.info.description}
+                width={60}
+                height={60}
+                unoptimized //reason for the 'unoptimized': https://github.com/vercel/next.js/issues/42032
+              />
+            </div>
+            <div className={styles.itemInfo}>
+              <div className={styles.name}>
+                <span className={styles.nameValue}>
+                  {item.info.name}
+                </span>
+              </div>
+              <div className={styles.collateral}>
+                <span className={styles.collateralValue}>0.01</span>
+                <span className={styles.collateralLabel}>Collateral</span>
+              </div>
+              <div className={styles.dailyTax}>
+                <span className={styles.dailyTaxValue}>0.01</span>
+                <span className={styles.dailyTaxLabel}>Daily Tax</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
