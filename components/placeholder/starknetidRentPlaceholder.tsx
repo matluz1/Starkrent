7a6e0f77b83c2371f7ac9e9999110eb71b4bb373
0xErenes <0xerenes@gmail.com>
2022-12-27 18:40:29 -0300

    add placeholder for rent page

---
+++ components/placeholder/starknetidRentPlaceholder.tsx
@@ -0,0 +1,12 @@
+const rentPlaceholder = [
+  {
+    id: '626200613416',
+    info: {
+      name: 'Starknet ID: 626200613416',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/626200613416',
+    },
+  },
+];
+
+export default rentPlaceholder;
