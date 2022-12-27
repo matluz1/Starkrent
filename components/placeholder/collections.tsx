e0dac94c87a25d9fd168a461fd20a13cf2107698
0xErenes <0xerenes@gmail.com>
2022-12-26 21:02:45 -0300

    change 'collectionInfo' to 'collections' and updates json structure

--- components/placeholder/collections.tsx
+++ components/placeholder/collections.tsx
@@ -1,53 +1,71 @@
 const collections = [
   {
-    name: 'Starknet.id',
-    profileImage: '/starknetid.jpg',
-    description: 'Identity and naming service for StarkNet',
-    address: '0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b', //testnet address
-    twitter: 'https://twitter.com/Starknet_id',
+    address:
+      '0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b', //testnet address
+    info: {
+      name: 'Starknet.id',
+      profileImage: '/starknetid.jpg',
+      description: 'Identity and naming service for StarkNet',
+      twitter: 'https://twitter.com/Starknet_id',
+    },
   },
   {
-    name: 'Another Collection',
-    profileImage: '/placeholder.png',
-    description:
-      'Vivamus in tincidunt tellus, a placerat felis. In id felis vel erat porta auctor at vel libero. Maecenas bibendum, massa.',
     address: '0xa3595A343CE794063864Efa845a75eacFaCf0F1E', //random address
-    discord: 'https://discord.com/anothercollection',
-    medium: 'https://medium.com/anothercollection',
+    info: {
+      name: 'Another Collection',
+      profileImage: '/placeholder.png',
+      description:
+        'Vivamus in tincidunt tellus, a placerat felis. In id felis vel erat porta auctor at vel libero. Maecenas bibendum, massa.',
+
+      discord: 'https://discord.com/anothercollection',
+      medium: 'https://medium.com/anothercollection',
+    },
   },
   {
-    name: 'Yet Another Collection',
-    profileImage: '/placeholder.png',
-    description:
-      'Praesent at ipsum sem. In hac habitasse platea dictumst. Vestibulum congue nulla ac euismod elementum. In in orci lacus. Vivamus.',
     address: '0x3b3f8a2B2A1BdE6A4b6B4A6A4c6Ae9bBc7Ae8bBd', //random address
-    twitter: 'https://twitter.com/yetanothercollection',
-    discord: 'https://discord.com/yetanothercollection',
+    info: {
+      name: 'Yet Another Collection',
+      profileImage: '/placeholder.png',
+      description:
+        'Praesent at ipsum sem. In hac habitasse platea dictumst. Vestibulum congue nulla ac euismod elementum. In in orci lacus. Vivamus.',
+
+      twitter: 'https://twitter.com/yetanothercollection',
+      discord: 'https://discord.com/yetanothercollection',
+    },
   },
   {
-    name: 'My Collection 4',
-    profileImage: '/placeholder.png',
-    description:
-      'Proin fringilla ultricies convallis. Praesent orci neque, laoreet ornare blandit et, volutpat quis massa. Ut scelerisque, sem vel convallis bibendum, diam est gravida sem, at tristique dui massa sed sem. Vivamus non bibendum neque. Sed congue, augue in faucibus rhoncus, elit nunc tincidunt nisi, non.',
     address: '0x671817E82fd2BD62Ad441d901077C03601d012De', //random address
-    twitter: 'https://twitter.com/mycollection4',
+    info: {
+      name: 'My Collection 4',
+      profileImage: '/placeholder.png',
+      description:
+        'Proin fringilla ultricies convallis. Praesent orci neque, laoreet ornare blandit et, volutpat quis massa. Ut scelerisque, sem vel convallis bibendum, diam est gravida sem, at tristique dui massa sed sem. Vivamus non bibendum neque. Sed congue, augue in faucibus rhoncus, elit nunc tincidunt nisi, non.',
+
+      twitter: 'https://twitter.com/mycollection4',
+    },
   },
   {
-    name: 'Another Collection 5',
-    profileImage: '/placeholder.png',
-    description: 'This is another collection of things',
     address: '0xa3595A343CE794053864Efa845a75eacFaCf0F1E', //random address
-    twitter: 'https://twitter.com/anothercollection5',
-    discord: 'https://discord.com/anothercollection5',
-    medium: 'https://medium.com/anothercollection5',
+    info: {
+      name: 'Another Collection 5',
+      profileImage: '/placeholder.png',
+      description: 'This is another collection of things',
+
+      twitter: 'https://twitter.com/anothercollection5',
+      discord: 'https://discord.com/anothercollection5',
+      medium: 'https://medium.com/anothercollection5',
+    },
   },
   {
-    name: 'Yet Another Collection 6',
-    profileImage: '/placeholder.png',
-    description: 'This is yet another collection of things',
     address: '0x3b3f8a2B2B1BdE6A4b6B4A6A4c6Ae9bBc7Ae8bBd', //random address
-    twitter: 'https://twitter.com/yetanothercollection6',
-    discord: 'https://discord.com/yetanothercollection6',
+    info: {
+      name: 'Yet Another Collection 6',
+      profileImage: '/placeholder.png',
+      description: 'This is yet another collection of things',
+
+      twitter: 'https://twitter.com/yetanothercollection6',
+      discord: 'https://discord.com/yetanothercollection6',
+    },
   },
 ];
 
