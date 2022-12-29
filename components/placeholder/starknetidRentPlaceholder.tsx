8d4b721e2042287136f0165bdee162c819fd4a64
0xErenes <0xerenes@gmail.com>
2022-12-28 21:16:01 -0300

    add new placeholder

--- components/placeholder/starknetidRentPlaceholder.tsx
+++ components/placeholder/starknetidRentPlaceholder.tsx
@@ -5,7 +5,7 @@ const rentPlaceholder = [
       name: 'Starknet ID: 626200613416',
       description: 'This token represents an identity on StarkNet.',
       image: 'https://starknet.id/api/identicons/626200613416',
-    }
+    },
   },
   {
     id: '4047446163',
@@ -13,7 +13,7 @@ const rentPlaceholder = [
       name: 'Starknet ID: 4047446163',
       description: 'This token represents an identity on StarkNet.',
       image: 'https://starknet.id/api/identicons/4047446163',
-    }
+    },
   },
   {
     id: '789793576154',
@@ -21,7 +21,7 @@ const rentPlaceholder = [
       name: 'Starknet ID: 789793576154',
       description: 'This token represents an identity on StarkNet.',
       image: 'https://starknet.id/api/identicons/789793576154',
-    }
+    },
   },
   {
     id: '261880110824',
@@ -29,8 +29,76 @@ const rentPlaceholder = [
       name: 'Starknet ID: 261880110824',
       description: 'This token represents an identity on StarkNet.',
       image: 'https://starknet.id/api/identicons/261880110824',
-    }
-  }
+    },
+  },
+  
+
+
+
+  {
+    id: '151714533694',
+    info: {
+      name: 'Starknet ID: 151714533694',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/151714533694',
+    },
+  },
+  {
+    id: '975565364964',
+    info: {
+      name: 'Starknet ID: 975565364964',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/975565364964',
+    },
+  },
+  {
+    id: '271151016753',
+    info: {
+      name: 'Starknet ID: 271151016753',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/271151016753',
+    },
+  },
+  {
+    id: '403599875324',
+    info: {
+      name: 'Starknet ID: 403599875324',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/403599875324',
+    },
+  },
+  {
+    id: '685666887468',
+    info: {
+      name: 'Starknet ID: 685666887468',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/685666887468',
+    },
+  },
+  {
+    id: '520669665935',
+    info: {
+      name: 'Starknet ID: 520669665935',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/520669665935',
+    },
+  },
+  {
+    id: '444190411650',
+    info: {
+      name: 'Starknet ID: 444190411650',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/444190411650',
+    },
+  },
+  {
+    id: '995420794050',
+    info: {
+      name: 'Starknet ID: 995420794050',
+      description: 'This token represents an identity on StarkNet.',
+      image: 'https://starknet.id/api/identicons/995420794050',
+    },
+  },
 ];
 
 export default rentPlaceholder;
