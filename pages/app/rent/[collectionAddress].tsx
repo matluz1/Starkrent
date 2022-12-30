1da2ad571e45d5679feddfefe5fc73b6025ac790
0xErenes <0xerenes@gmail.com>
2022-12-30 18:42:16 -0300

    add itemContent

--- pages/app/rent/[collectionAddress].tsx
+++ pages/app/rent/[collectionAddress].tsx
@@ -16,52 +16,54 @@ export default function Page() {
     <>
       <div className={styles.collectionItemWrapper}>
         {rentPlaceholder.map((item) => (
-          <button className={styles.collectionItem} key={item.id}>
-            <div
-              className={
-                notFullImage
-                  ? `${styles.itemImage} ${styles.notFullImage}`
-                  : styles.itemImage
-              }
-            >
-              <Image
-                src={item.info.image}
-                alt={item.info.description}
-                width={60}
-                height={60}
-                unoptimized //reason for the 'unoptimized': https://github.com/vercel/next.js/issues/42032
-              />
-            </div>
-            <div className={styles.itemInfo}>
-              <div className={styles.name}>
-                <span className={styles.nameValue}>{item.info.name}</span>
+          <div className={styles.collectionItem} key={item.id}>
+            <button className={styles.itemContent}>
+              <div
+                className={
+                  notFullImage
+                    ? `${styles.itemImage} ${styles.notFullImage}`
+                    : styles.itemImage
+                }
+              >
+                <Image
+                  src={item.info.image}
+                  alt={item.info.description}
+                  width={60}
+                  height={60}
+                  unoptimized //reason for the 'unoptimized': https://github.com/vercel/next.js/issues/42032
+                />
               </div>
-              <div className={styles.collateral}>
-                <div className={styles.collateralValue}>
-                  <span>15</span>
-                  <Image
-                    src="/ethereum.svg"
-                    alt="Ethereum logo"
-                    width={15}
-                    height={15}
-                  />
+              <div className={styles.itemInfo}>
+                <div className={styles.name}>
+                  <span className={styles.nameValue}>{item.info.name}</span>
                 </div>
-                <span className={styles.collateralLabel}>Collateral</span>
-              </div>
-              <div className={styles.dailyTax}>
-                <div className={styles.collateralValue}>
-                  <span>0.01</span>
-                  <Image
-                    src="/ethereum.svg"
-                    alt="Ethereum logo"
-                    width={15}
-                    height={15}
-                  />
+                <div className={styles.collateral}>
+                  <div className={styles.collateralValue}>
+                    <span>15</span>
+                    <Image
+                      src="/ethereum.svg"
+                      alt="Ethereum logo"
+                      width={15}
+                      height={15}
+                    />
+                  </div>
+                  <span className={styles.collateralLabel}>Collateral</span>
                 </div>
-                <span className={styles.dailyTaxLabel}>Daily Tax</span>
+                <div className={styles.dailyTax}>
+                  <div className={styles.collateralValue}>
+                    <span>0.01</span>
+                    <Image
+                      src="/ethereum.svg"
+                      alt="Ethereum logo"
+                      width={15}
+                      height={15}
+                    />
+                  </div>
+                  <span className={styles.dailyTaxLabel}>Daily Tax</span>
+                </div>
               </div>
-            </div>
-          </button>
+            </button>
+          </div>
         ))}
       </div>
     </>
