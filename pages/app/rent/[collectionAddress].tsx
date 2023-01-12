9e1e9829d355b344e30f49c3531c8a6389ced66f
0xErenes <0xerenes@gmail.com>
2023-01-12 01:52:03 -0300

    fix things

--- pages/app/rent/[collectionAddress].tsx
+++ pages/app/rent/[collectionAddress].tsx
@@ -1,19 +1,27 @@
+import { useEffect } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
-import { useStarknetExecute } from '@starknet-react/core';
+import axios from 'axios';
+import {
+  useAccount,
+  useContract,
+  useStarknetCall,
+  useStarknetExecute,
+} from '@starknet-react/core';
 import styles from '../../../styles/[collectionAddress].module.scss';
-import rentPlaceholder from '../../../placeholder/starknetidRentPlaceholder.json';
 import collections from '../../../placeholder/collections.json';
+import rentPlaceholder from '../../../placeholder/starkIdFolderStarknetBucketS3.json';
+import starknetIdAbi from '../../../abi/starknetId.abi.json';
+import starknet101Abi from '../../../abi/starknet101.abi.json';
 
-export default function Page() {
-  const router = useRouter();
-  const { collectionAddress } = router.query;
-  const ethIconSize = 15;
+async function fetchData() {
+  const contractStruct = await axios.get(
+    '/api/collection/rental/0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b',
+  );
+  console.log(contractStruct.data.contractRental);
+}
 
-  const notFullImage = collections.find(
-    (item) => item.address === collectionAddress,
-  )?.info.notFullImageItems;
-
+function getExecuteMethod() {
   //run reset_counter method from https://github.com/starknet-edu/starknet-cairo-101/blob/main/contracts/ex03.cairo
   const calls = [
     {
@@ -24,10 +32,49 @@ export default function Page() {
     },
   ];
 
-  const { execute } = useStarknetExecute({ calls });
+  return useStarknetExecute({ calls }).execute;
+}
+
+function getTokenUri() {}
+
+export default function Page() {
+  const router = useRouter();
+  const { collectionAddress } = router.query;
+  const ethIconSize = 15;
+
+  const notFullImage = collections.find(
+    (item) => item.address === collectionAddress,
+  )?.info.notFullImageItems;
+
+  useEffect(() => {
+    
+    fetchData();
+  }, []);
+
+  const execute = getExecuteMethod();
+
+  //console.log tokenURI from id
+  // const { contract } = useContract({
+  //   address:
+  //     '0x0798e884450c19e072d6620fefdbeb7387d0453d3fd51d95f5ace1f17633d88b',
+  //   abi: starknetIdAbi,
+  // });
+  // const { address } = useAccount();
+  // const { data, loading, error, refresh } = useStarknetCall({
+  //   contract,
+  //   method: 'name',
+  //   args: [],
+  //   options: {
+  //     watch: false,
+  //   },
+  // });
+
 
   return (
     <>
+      {/* {loading && <span>Loading...</span>}
+      {error && <span>Error: {error}</span>}
+      {error && <span>Data: {JSON.stringify(data)}</span>} */}
       <div className={styles.collectionItemWrapper}>
         {rentPlaceholder.map((item) => (
           <div className={styles.collectionItem} key={item.id}>
@@ -40,8 +87,8 @@ export default function Page() {
                 }
               >
                 <Image
-                  src={item.info.image}
-                  alt={item.info.description}
+                  src={item.metadata.image}
+                  alt={item.metadata.description}
                   width={60}
                   height={60}
                   unoptimized //reason for the 'unoptimized': https://github.com/vercel/next.js/issues/42032
@@ -49,7 +96,7 @@ export default function Page() {
               </div>
               <div className={styles.itemInfo}>
                 <div className={styles.name}>
-                  <span className={styles.nameValue}>{item.info.name}</span>
+                  <span className={styles.nameValue}>{item.metadata.name}</span>
                 </div>
                 <div className={styles.collateral}>
                   <div className={styles.collateralValue}>
@@ -81,7 +128,7 @@ export default function Page() {
               <span>Borrow</span>
             </button>
             <div className={styles.dayMinMax}>
-              <span>day min - days max</span>
+              <span>{}</span>
             </div>
           </div>
         ))}
