a72374906d2d811a943437180ea98eaaf458cf07
0xErenes <0xerenes@gmail.com>
2023-02-13 15:57:40 -0300

    add OfferContract interface

--- utils/writeBlockchainInfo.ts
+++ utils/writeBlockchainInfo.ts
@@ -1,9 +1,6 @@
 import { useStarknetExecute } from '@starknet-react/core';
 
-
-interface ContractOffer {
-  index: number;
-  owner: string;
+export interface OfferContract {
   collection: string;
   tokenId: string;
   collateral: string;
@@ -11,7 +8,45 @@ interface ContractOffer {
   interest_rate: number;
   rent_time_min: number;
   rent_time_max: number;
+}
+
+export interface RentContract extends OfferContract {
+  index: number;
+  owner: string;
   timestamp: number;
+}
+
+export function getOfferExecute({
+  collection,
+  tokenId,
+  collateral,
+  collateral_amount,
+  interest_rate,
+  rent_time_min,
+  rent_time_max,
+}: OfferContract) {
+  const starkrentContract =
+    '0x02a28030a1b1166e5e66bb25abe2bea61b31a21a1be791386210ad4a901c2275'; //testnet contract
+  const calls = [
+    {
+      contractAddress: starkrentContract,
+      entrypoint: 'offer',
+      calldata: [
+        collection,
+        tokenId, //cairo Uint256
+        0,
+        collateral,
+        collateral_amount, //cairo Uint256
+        0,
+        interest_rate, //cairo Uint256
+        0,
+        rent_time_min,
+        rent_time_max,
+      ],
+    },
+  ];
+
+  return useStarknetExecute({ calls }).execute;
 }
 
 export function getRentExecute({
@@ -24,8 +59,7 @@ export function getRentExecute({
   rent_time_min,
   rent_time_max,
   timestamp,
-}: ContractOffer) {
-
+}: RentContract) {
   const starkrentContract =
     '0x02a28030a1b1166e5e66bb25abe2bea61b31a21a1be791386210ad4a901c2275'; //testnet contract
   const calls = [
@@ -44,41 +78,7 @@ export function getRentExecute({
         0,
         rent_time_min,
         rent_time_max,
-        timestamp
-      ],
-    },
-  ];
-
-  return useStarknetExecute({ calls }).execute;
-}
-
-export function getOfferExecute({
-  collection,
-  tokenId,
-  collateral,
-  collateral_amount,
-  interest_rate,
-  rent_time_min,
-  rent_time_max,
-}: any) {
-
-  const starkrentContract =
-    '0x02a28030a1b1166e5e66bb25abe2bea61b31a21a1be791386210ad4a901c2275'; //testnet contract
-  const calls = [
-    {
-      contractAddress: starkrentContract,
-      entrypoint: 'offer',
-      calldata: [
-        collection,
-        tokenId, //cairo Uint256
-        0,
-        collateral,
-        collateral_amount * 0.1, //cairo Uint256
-        0,
-        interest_rate * 0.1, //cairo Uint256
-        0,
-        rent_time_min,
-        rent_time_max,
+        timestamp,
       ],
     },
   ];
