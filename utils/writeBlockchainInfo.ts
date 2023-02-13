705ab9418e93b85665375b557295e616fcb68563
0xErenes <0xerenes@gmail.com>
2023-02-13 15:25:52 -0300

    add offer and rent contract interactions

--- utils/writeBlockchainInfo.ts
+++ utils/writeBlockchainInfo.ts
@@ -38,13 +38,47 @@ export function getRentExecute({
         tokenId, //cairo Uint256
         0,
         collateral,
+        collateral_amount, //cairo Uint256
+        0,
+        interest_rate, //cairo Uint256
+        0,
+        rent_time_min,
+        rent_time_max,
+        timestamp
+      ],
+    },
+  ];
+
+  return useStarknetExecute({ calls }).execute;
+}
+
+export function getOfferExecute({
+  collection,
+  tokenId,
+  collateral,
+  collateral_amount,
+  interest_rate,
+  rent_time_min,
+  rent_time_max,
+}: any) {
+
+  const starkrentContract =
+    '0x02a28030a1b1166e5e66bb25abe2bea61b31a21a1be791386210ad4a901c2275'; //testnet contract
+  const calls = [
+    {
+      contractAddress: starkrentContract,
+      entrypoint: 'offer',
+      calldata: [
+        collection,
+        tokenId, //cairo Uint256
+        0,
+        collateral,
         collateral_amount * 0.1, //cairo Uint256
         0,
         interest_rate * 0.1, //cairo Uint256
         0,
         rent_time_min,
         rent_time_max,
-        timestamp
       ],
     },
   ];
