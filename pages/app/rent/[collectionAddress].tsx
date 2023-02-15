a1aad8e5abae7bfa59e21b51496786eac82190c4
0xErenes <0xerenes@gmail.com>
2023-02-15 14:52:17 -0300

    add offered argument

--- pages/app/rent/[collectionAddress].tsx
+++ pages/app/rent/[collectionAddress].tsx
@@ -77,6 +77,7 @@ export default function Page() {
             key={element.tokenId}
             nftOffer={element}
             fullImage={fullImage}
+            offered
           />
         ))}
     </div>
