0018fd00c3bbfd554da4bf3c3652279ab70b0d7d
0xErenes <0xerenes@gmail.com>
2023-04-17 18:34:24 -0300

    add owned nft visualization

--- pages/app/profile.tsx
+++ pages/app/profile.tsx
@@ -6,10 +6,25 @@ import NftCard from '../../components/nftCard';
 import Listbox from '../../components/listbox';
 import { getUserRents, getMetadata } from '../../utils/readBlockchainInfo';
 import { IndexedRentContract } from '../../utils/starkrentInterfaces';
+import { getUserAssetsFromCollection } from '../../utils/readBlockchainInfo';
 
 interface NftInfo {
   rentInfo: IndexedRentContract;
   metadata: any;
+}
+
+interface OwnedNft {
+  owner: string;
+  collection: string;
+  tokenId: string;
+  metadata: any;
+}
+
+interface Metadata {
+  name: string;
+  image: string;
+  description: string;
+  attributes: any;
 }
 
 const categories = [
@@ -32,31 +47,41 @@ function getProfileContent(userAddress: string) {
   </div>
 }
 
-function getNftCards(nftInfoArray: NftInfo[], category: string) {
-  let nftCards;
-  if (category === "Rented") {
-  nftCards = <div className={styles.collectionItemWrapper}> {nftInfoArray.map((element) => (
-    <NftCard
-      key={element.rentInfo.index}
-      metadata={element.metadata}
-      rentInfo={element.rentInfo}
-      fullImage={false}
-    />
-  ))}</div>;
-  } 
-
-  return nftCards;
-}
-
 export default function Profile() {
   const { status: userStatus, address: userAddress } = useAccount();
 
   const [nftInfoArray, setNftInfoArray] = useState<NftInfo[]>([]);
+  const [ownedNftArray, setOwnedNftArray] = useState<OwnedNft[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [selectedCategory, setSelectedCategory] = useState(categories[0]);
 
   function getMenu() {
     return <Listbox categories={categories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+  }
+
+  function getNftCards(category: string) {
+    let nftCards;
+    if (category === "Rented") {
+    nftCards = <div className={styles.collectionItemWrapper}> {nftInfoArray.map((element) => (
+      <NftCard
+        key={element.rentInfo.index}
+        metadata={element.metadata}
+        rentInfo={element.rentInfo}
+        fullImage={false}
+      />
+    ))}</div>;
+    }
+    if (category === "Owned") {
+      nftCards = <div className={styles.collectionItemWrapper}> {ownedNftArray.map((element) => (
+        <NftCard
+          key={element.tokenId}
+          metadata={element.metadata}
+          fullImage={false}
+        />
+      ))}</div>;
+      }
+  
+    return nftCards;
   }
 
   async function fetchAsync() {
@@ -70,7 +95,24 @@ export default function Profile() {
         return { rentInfo: element, metadata };
       }),
     );
+    const collection = "0x0783a9097b26eae0586373b2ce0ed3529ddc44069d1e0fbc4f66d42b69d6850d"; //starkid
+    const userAssets = await getUserAssetsFromCollection(collection, userAddress || '');
+    const ownedNfts = userAssets.assets.map((element: any) => {
+      const metadata: Metadata = {
+        name: element.name,
+        image: element.image_small_url_copy,
+        description: element.description,
+        attributes: element.attributes
+      }
+      return { 
+        owner: element.owner.accountAddress,
+        collection: element.contract.contract_address,
+        tokenId: element.token_id,
+        metadata
+      }
+    } )
     setNftInfoArray(rentalAndMetadataArray);
+    setOwnedNftArray(ownedNfts);
     setIsLoading(false);
   }
 
@@ -85,7 +127,7 @@ export default function Profile() {
       {userStatus == 'disconnected' && getConnectWallet()}
       {userAddress && [getProfileContent(userAddress), getMenu()]}
       {isLoading && userStatus == 'connected' && getLoading()}
-      {!isLoading && userStatus == 'connected' && getNftCards(nftInfoArray, selectedCategory.name)}
+      {!isLoading && userStatus == 'connected' && getNftCards(selectedCategory.name)}
     </section>
   );
 }
